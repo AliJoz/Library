de2ee1e9b8a79328217e5c0d998fc2409d732fc4
Alireza Joz <alireza00joz@gmail.com>
2024-09-26 14:22:20 +0330

    🎨  Edite user and get user and book

--- code/Libaray.js
+++ code/Libaray.js
@@ -1,29 +1,116 @@
-const fs = require("fs");
 const http = require("http");
+const fs = require("fs");
+const url = require("url");
+const db = require("./db.json");
+const { log } = require("console");
 
-const serve = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/Api/Book") {
-    fs.readFile("db.json", (err, data) => {
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url === "/api/users") {
+    fs.readFile("db.json", (err, db) => {
       if (err) {
-        // در صورت بروز خطا، پاسخ خطا را ارسال کنید
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Internal Server Error");
-        return;
+        throw err;
       }
-      const datas = JSON.parse(data);
-      // تبدیل داده JSON به آبجکت و ارسال آن به عنوان رشته
+
+      const data = JSON.parse(db);
+
       res.writeHead(200, { "Content-Type": "application/json" });
-      res.write(JSON.stringify(datas.books));
-   
+      res.write(JSON.stringify(data.users));
       res.end();
     });
-  } else {
-    // پاسخ برای درخواست‌های دیگر
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
+  } else if (req.method === "GET" && req.url === "/api/books") {
+    fs.readFile("db.json", (err, db) => {
+      if (err) {
+        throw err;
+      }
+
+      const data = JSON.parse(db);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(data.books));
+      res.end();
+    });
+  } else if (req.method === "DELETE" && req.url.startsWith("/api/books")) {
+    const parsedUrl = url.parse(req.url, true);
+    const bookID = parsedUrl.query.id;
+
+    const newBooks = db.books.filter((book) => book.id != bookID);
+
+    if (newBooks.length === db.books.length) {
+      res.writeHead(401, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ message: "Book Not Found" }));
+      res.end();
+    } else {
+      fs.writeFile(
+        "db.json",
+        JSON.stringify({ ...db, books: newBooks }),
+        (err) => {
+          if (err) {
+            throw err;
+          }
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.write(JSON.stringify({ message: "Book Removed Successfully" }));
+          res.end();
+        }
+      );
+    }
+  } else if (req.method === "POST" && req.url === "/api/books") {
+    let book = "";
+
+    req.on("data", (data) => {
+      book = book + data.toString();
+    });
+
+    req.on("end", () => {
+      const newBook = { id: crypto.randomUUID(), ...JSON.parse(book), free: 1 };
+
+      db.books.push(newBook);
+
+      fs.writeFile("db.json", JSON.stringify(db), (err) => {
+        if (err) {
+          throw err;
+        }
+
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.write(JSON.stringify({ message: "New Book Added Successfully" }));
+        res.end();
+      });
+    });
+  }
+  // Edite
+  else if (req.method === "PUT" && req.url.startsWith("/api/books")) {
+    const parseurl = url.parse(req.url, true);
+    const bookId = parseurl.query.id;
+
+    let BookNewid = "";
+    req.on("data", (data) => {
+      BookNewid = BookNewid + data.toString();
+      console.log(BookNewid);
+    });
+    req.on("end", () => {
+      const reqBody = JSON.parse(BookNewid);
+
+      db.books.forEach((data) => {
+        if (data.id === Number(bookId)) {
+          data.title = reqBody.title;
+          data.author = reqBody.author; // احتمالا باید 'author' باشد
+          data.price = reqBody.price;
+        }
+      });
+    });
+    fs.writeFile("./db.json", JSON.stringify(db), (err) => {
+      if (err) {
+        throw err;
+      } else {
+        res.writeHead(201, { "Content-Type": "application/json" });
+        res.end(JSON.stringify({ massage: "Successfully Uppdate Books" }));
+        
+      }
+    });
+    console.log(db)
   }
 });
 
-serve.listen(4201, () => {
-  console.log("Server running on port 4201");
+server.listen(4000, () => {
+  console.log("Server Rinning On Port 4000");
 });
